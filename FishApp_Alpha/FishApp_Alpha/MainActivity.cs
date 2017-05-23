e891bfb5443ae75ec9ed4a906b27d9101ba4431d
rayaqin <rayaqin@gmail.com>
2017-05-23 23:17:09 +0200

    5+5
    10

---
+++ FishApp_Alpha/FishApp_Alpha/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System.Threading;
+
+
+namespace FishApp_Alpha
+{
+    [Activity(Label = "FishApp_Alpha", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+
+            
+
+
+
+            
+
+
+            SetContentView(Resource.Layout.Categories);
+
+        }
+
+         
+
+
+
+
+    }
+
+
+    
+
+
+
+}
+
+
+
+
+
+
+
+
+
---
+++ FishApp_Alpha/FishApp_Alpha/obj/Debug/android/src/md537c672c66c50340e6715c40637064172/Categories.java
@@ -0,0 +1,62 @@
+package md537c672c66c50340e6715c40637064172;
+
+
+public class Categories
+	extends android.view.View
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("FishApp_Alpha.Categories, FishApp_Alpha, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Categories.class, __md_methods);
+	}
+
+
+	public Categories (android.content.Context p0) throws java.lang.Throwable
+	{
+		super (p0);
+		if (getClass () == Categories.class)
+			mono.android.TypeManager.Activate ("FishApp_Alpha.Categories, FishApp_Alpha, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public Categories (android.content.Context p0, android.util.AttributeSet p1) throws java.lang.Throwable
+	{
+		super (p0, p1);
+		if (getClass () == Categories.class)
+			mono.android.TypeManager.Activate ("FishApp_Alpha.Categories, FishApp_Alpha, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:Android.Util.IAttributeSet, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0, p1 });
+	}
+
+
+	public Categories (android.content.Context p0, android.util.AttributeSet p1, int p2) throws java.lang.Throwable
+	{
+		super (p0, p1, p2);
+		if (getClass () == Categories.class)
+			mono.android.TypeManager.Activate ("FishApp_Alpha.Categories, FishApp_Alpha, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:Android.Util.IAttributeSet, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public Categories (android.content.Context p0, android.util.AttributeSet p1, int p2, int p3) throws java.lang.Throwable
+	{
+		super (p0, p1, p2, p3);
+		if (getClass () == Categories.class)
+			mono.android.TypeManager.Activate ("FishApp_Alpha.Categories, FishApp_Alpha, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:Android.Util.IAttributeSet, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1, p2, p3 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
